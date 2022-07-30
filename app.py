3ee6b4a6c59a88b77fcf6fe4df07b7508e300b02
linoqui14 <56160486+linoqui14@users.noreply.github.com>
2022-07-30 13:35:02 +0800

    for deployment part 2

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run(threaded=True, port=5000)
+    app.run()
