1994af81b92063499fc05593b3b615d78362d0f9
linoqui14 <56160486+linoqui14@users.noreply.github.com>
2022-07-30 14:56:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def getReviewData(
             soup = html_code(url)
             cus_res = cus_data(soup)
             page+=1
-            return soup
+            return str(soup)
             if cus_res == 'n/a':
                 continue
             if len(cus_res) == 0:
