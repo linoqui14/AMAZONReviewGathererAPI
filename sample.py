73abea943d31f5b663985de145e0f0387e785ac9
linoqui14 <56160486+linoqui14@users.noreply.github.com>
2022-09-03 23:27:50 +0800

    sa

--- sample.py
+++ sample.py
@@ -9,26 +9,26 @@ from endecrypt import asciisize,deasciisize
 sampleurl = 'https://shopee.ph/2022-Best-selling-Korean-ladies-white-shoes-i.174274877.7867509069?sp_atk=a3166c4c-9041-418d-bbe5-6c43a41749c9&xptdk=a3166c4c-9041-418d-bbe5-6c43a41749c9s'
 sampleurl_zmazon = 'https://www.amazon.com/AmLactin-Moisturizing-Lotion-Bottle-Paraben/product-reviews/B07BRDVQ4B?reviewerType=all_reviews'
 # #convert to ascii
-ascii_url = asciisize(sampleurl)
+ascii_url = asciisize(sampleurl_zmazon)
 r = requests.get('https://argatherer.herokuapp.com/'+ascii_url)#append it to url request
 
 json_objects = json.loads(r.content)#get the content of request
 reviews = []#empty list
 print(json_objects)
-# for x in json_objects:#alterate all json object from json_objects
-#     review = Review.to_Object(json_objects[x])#create a review_model object from that json using to_Object
-#     reviews.append(review)#append it to the list
+for x in json_objects:#alterate all json object from json_objects
+    review = Review.to_Object(json_objects[x])#create a review_model object from that json using to_Object
+    reviews.append(review)#append it to the list
 
-# for review in reviews:#alterate all reviews
-#     #so list of reviews man imu nakuha
-#     #every review is an individual review sa user
-#     #ato siya ge butang sa list
-#     #then you can just say
-#     print(review.name)#for example
-#     print(review.title)#for example
-#     print(review.comment)
-#     print(review.sentiment)
-#     print('-------------------------------')
+for review in reviews:#alterate all reviews
+    #so list of reviews man imu nakuha
+    #every review is an individual review sa user
+    #ato siya ge butang sa list
+    #then you can just say
+    print(review.name)#for example
+    print(review.title)#for example
+    print(review.comment)
+    print(review.sentiment)
+    print('-------------------------------')
 
 
 
